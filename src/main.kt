a1559580f2d362ae8ba4c3b54ecf0164906624bd
gustavo <gustavo.maia@b2b.com.pt>
2020-11-16 09:15:31 +0000

    Restrições das variáveis

--- src/main.kt
+++ src/main.kt
@@ -3,8 +3,13 @@ fun main(){
     println("Bem vindo ao Bytebank")
 
     val titular = "Milo"
+    val numeroConta = 23486
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
 
     println("Titular $titular")
-
+    println("Numero da conta $numeroConta")
+    println("Saldo da conta $saldo")
 
 }
