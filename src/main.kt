277e44efa1f4ce4e08f6937357dc224019b8b99e
gustavo <gustavo.maia@b2b.com.pt>
2020-11-16 09:48:30 +0000

    Utilizando estrutura de repetição

--- src/main.kt
+++ src/main.kt
@@ -2,16 +2,27 @@
 fun main(){
     println("Bem vindo ao Bytebank")
 
-    val titular = "Milo"
-    val numeroConta = 23486
-    var saldo = 0.0
-    saldo = 100 + 2.0
-    saldo += 200
+    for(i in 1..5 step 1) {
 
-    println("Titular $titular")
-    println("Numero da conta $numeroConta")
-    println("Saldo da conta $saldo")
+        if(i == 2){
+            continue
+        }
 
+        val titular = "Milo $i"
+        val numeroConta = 2000 + i
+        var saldo =  i + 2.0
+
+        println("Titular $titular")
+        println("Numero da conta $numeroConta")
+        println("Saldo da conta $saldo")
+
+        testaCondicoes(saldo)
+        println()
+    }
+
+}
+
+fun testaCondicoes(saldo : Double){
 
     when {
         saldo > 0.0 -> println("Saldo positivo")
@@ -19,5 +30,4 @@ fun main(){
         else ->  println("Saldo negativo")
 
     }
-
 }
