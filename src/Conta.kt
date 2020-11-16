c472a945cdad3b6c06c5aac6c108698dde35bb56
gustavo <gustavo.maia@b2b.com.pt>
2020-11-16 15:37:13 +0000

    Extração de classe

---
+++ src/Conta.kt
@@ -0,0 +1,30 @@
+class Conta(var titular: String, val numero: Int) {
+
+    var saldo = 0.0
+        private set
+
+    fun deposita(valor : Double){
+        if(valor > 0) {
+            this.saldo += valor
+        }
+    }
+
+    fun levantamento(valor : Double){
+
+        if(saldo >= valor){
+            this.saldo -= valor
+        }
+    }
+
+    fun transferir(valor : Double, contaDestino : Conta): Boolean {
+        if(saldo >= valor){
+            saldo -= valor
+            contaDestino.deposita(valor)
+            return true
+        }
+        else{
+            return false
+        }
+    }
+
+}
