a167492cd7ee5ffeb68b5ddfec73f536791e73e3
gustavo <gustavo.maia@b2b.com.pt>
2020-11-16 15:43:05 +0000

    Criando uma nova classe

---
+++ src/Funcionario.kt
@@ -0,0 +1,7 @@
+class Funcionario (val nome : String, val cpf : String, val salario : Double){
+
+    fun bonificacao(): Double {
+        return salario * 0.1
+    }
+
+}
--- src/main.kt
+++ src/main.kt
@@ -3,38 +3,8 @@ fun main(){
     println("Bem vindo ao Bytebank")
 
     val contaYoga = Conta(titular = "Yoga", numero = 100)
-    contaYoga.deposita(500.0)
 
-    println(contaYoga.titular)
-    println(contaYoga.saldo)
+    val yoga = Funcionario("Yoga", "333.22.11.55", 2500.0)
 
-    val contaShiryu = Conta(numero =  200, titular = "Shiryu")
-    contaYoga.deposita(1500.0)
-
-    println(contaShiryu.titular)
-    println(contaShiryu.saldo)
-
-    println("Depósito na conta do Yoga")
-    contaYoga.deposita(50.0)
-    println(contaYoga.saldo)
-
-    println("Depósito na conta do Shiryu")
-    contaShiryu.deposita(150.0)
-    println(contaShiryu.saldo)
-
-    println("Levantamento na conta do Yoga")
-    contaYoga.levantamento(50.0)
-    println(contaYoga.saldo)
-
-    println("Transferencia do Yoga para o Shiryu")
-    if(contaYoga.transferir(100.0, contaShiryu)){
-        println("Transferencia realizada com sucesso")
-    }
-    else{
-        println("Transferencia não realizada")
-    }
-
-    println(contaYoga.saldo)
-    println(contaShiryu.saldo)
-
+    println("Nome: ${yoga.nome} bonificacao: ${yoga.bonificacao()}")
 }
