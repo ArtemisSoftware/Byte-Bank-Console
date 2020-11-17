3bbfc5b2ad4c5b014d4f64fb278d8a37141545bd
gustavo <gustavo.maia@b2b.com.pt>
2020-11-17 09:57:00 +0000

    Herdando mais de uma classe

--- src/FuncionarioAdmin.kt
+++ src/FuncionarioAdmin.kt
@@ -1,6 +1,4 @@
-abstract class FuncionarioAdmin (val nome : String, val cpf : String, val salario : Double, val  senha : Int){
-
-    abstract val bonificacao: Double
+abstract class FuncionarioAdmin (nome : String, cpf : String, salario : Double, val  senha : Int) : Funcionario(nome, cpf, salario){
 
     fun autenticacar(senha : Int): Boolean {
         return (this.senha == senha)
