3b452a216d0d4f3b4081b61f36f6d51f7eed7d63
gustavo <gustavo.maia@b2b.com.pt>
2020-11-19 10:06:27 +0000

    Lançando exceptions

--- src/com/artemis/software/main.kt
+++ src/com/artemis/software/main.kt
@@ -1,40 +1,21 @@
 package com.artemis.software
 
 import com.artemis.software.modelos.*
-
+import java.lang.ClassCastException
 
 
 fun main(){
 
     println("início main")
 
-    val entrada: String = "1,9"
-
-
-    val valor: Double? = try{
-        entrada.toDouble()
+    try{
+        funcao2()
     }
-    catch(e: NumberFormatException){
-        println("NumberFormatException message: ${e.message}")
-        println("NumberFormatException cause: ${e.cause}")
-        null
+    catch (e : ClassCastException){
+        e.printStackTrace()
+        println("ClassCastException")
     }
 
-    println("Valor final: ${valor}")
-
-
-    val valorComTaxa : Double? = if(valor != null){
-        valor + 1
-    }
-    else{
-        null
-    }
-
-    println("valorComTaxa final: ${valorComTaxa}")
-
-
-    funcao1()
-    println("fim main")
 }
 
 fun funcao1(){
@@ -47,7 +28,9 @@ fun funcao2() {
     println("início funcao2")
     for (i in 1..5){
         println(i)
-        
+        val endereco = Any()
+        throw ClassCastException()
+
     }
     println("fim funcao2")
 }
