92ca7cfcb4b18ae284529b50d8246822087fc97b
gustavo <gustavo.maia@b2b.com.pt>
2020-11-19 09:51:24 +0000

    Lidando com exceptions com o try expression

--- src/com/artemis/software/main.kt
+++ src/com/artemis/software/main.kt
@@ -6,32 +6,30 @@ import com.artemis.software.modelos.*
 
 fun main(){
 
-    val shiryu = Gerente("Shiryu", "34534-5443.22", 1000.0, 1234)
-    val ikki = Diretor("Ikki", "7766-444.33.2", 100000.0, 1234, 11.0)
-    val yoga = Gerente("Yoga", "333.22.11.55", 2500.0, 1234)
-    val joao = Cliente("joao", "222-444.232", Endereco(), 4321)
-    val maria = Cliente("maria", "222-444.232", Endereco(),4321)
+    println("início main")
 
-    val contaJoao = ContaCorrente(joao, 1000)
-    val contaPoupanca = ContaPoupanca(maria, 1001)
+    try{
+        10/0
+    }
+    catch(e: ArithmeticException){
+        println("ArithmeticException ")
+    }
 
-    println("Nome do titular da conta: ${contaJoao.titular.nome}")
-    println("Endereço do titular da conta: ${contaJoao.titular.endereco.cidade}")
+    funcao1()
+    println("fim main")
+}
 
-    val sistemaInterno = SistemaInterno()
+fun funcao1(){
+    println("início funcao1")
+    funcao2()
+    println("fim funcao1")
+}
 
-    sistemaInterno.iniciarSessao(shiryu, 1234)
-    sistemaInterno.iniciarSessao(joao, 1234)
-
-    println("Total de contas: ${Conta.total}")
-
-    val endereco1 = Endereco(logradouro = "Em cima", cep = "234")
-    val endereco2 = Endereco(logradouro = "Em baixo", cep = "234")
-
-    println("Endereço: ${endereco1}")
-
-    println("endereco1 hashcode : ${endereco1.hashCode()}")
-    println("endereco2 hashcode : ${endereco2.hashCode()}")
-    println("Endereços iguais: ${endereco1.equals(endereco2)}")
-
+fun funcao2() {
+    println("início funcao2")
+    for (i in 1..5){
+        println(i)
+        
+    }
+    println("fim funcao2")
 }
